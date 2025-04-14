298738ede7b3245fca88c38b0ad739ff731b649f
shallon01 <shallonurujeni@gmail.com>
2025-04-14 23:33:14 +0200

    register blade added

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,6 @@
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
         <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
             <h2 class="text-2xl font-bold text-center mb-4">Register</h2>
-
             <form method="POST" action="{{ route('register') }}">
                 @csrf
 
